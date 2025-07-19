493a96c76a0885ca63be3fd8f3b3fc89feb7cbc8
65NinadA3 <ninadphatak123@gmail.com>
2025-07-19 19:13:29 +0530

    Create To check whether a number is even or odd.cpp

---
+++ To check whether a number is even or odd.cpp
@@ -0,0 +1,18 @@
+//Ninad Phatak
+//24070123065
+//ENTC A3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int j;
+    cout << "Enter the number: ";
+    cin >> j;
+    if (j % 2 == 0) {
+        cout << "The given number is even." << endl;
+    } else {
+        cout << "The given number is odd." << endl;
+    }
+
+    return 0;
+}
